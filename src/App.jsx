80b62af11faaeb79ecc6b82e22cbba3f60fb5f98
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-23 19:44:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+
+      <Container></Container>
+    </>
+  );
+}
+
+export default App;
