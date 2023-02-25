1a81a5171923e7722f2fbec030a6e36c01453635
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-24 21:47:55 -0300

    Criar componente Card para apresentar as bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,5 +1,21 @@
+import { Col, Card, Button } from "react-bootstrap";
+
 const Bebida = ({ bebida }) => {
-  return <div>{bebida.strDrink}</div>;
+  return (
+    <Col md={6} lg={3}>
+      <Card className="mb-4">
+        <Card.Img
+          variant="top"
+          src={bebida.strDrinkThumb}
+          alt={`Imagen de ${bebida.strDrink}`}
+        />
+        <Card.Body>
+          <Card.Title>{bebida.strDrink}</Card.Title>
+          <Button className="w-100 text-uppercase mt-2">Ver Receta</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default Bebida;
